3ac696edb4b66e9ed50ba7e582629783e3047f35
Manh Cuong <cuong2th2@gmail.com>
2023-04-09 00:37:21 +0700

    edit and delete specialty

--- src/specialty/specialty.service.ts
+++ src/specialty/specialty.service.ts
@@ -4,6 +4,7 @@ import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Specialty } from './entities/specialty.entity';
 import { Repository } from 'typeorm';
+import * as fs from 'fs';
 
 @Injectable()
 export class SpecialtyService {
@@ -38,15 +39,84 @@ export class SpecialtyService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} specialty`;
+  async findOne(id: number) {
+    try {
+      if (!id) {
+        return {
+          statusCode: 1,
+          message: 'Chưa nhập id',
+        };
+      }
+      const res = await this.specialtyRepository.findOneBy({ id });
+      return {
+        statusCode: 0,
+        message: 'OK',
+        spec: res,
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  update(id: number, updateSpecialtyDto: UpdateSpecialtyDto) {
-    return `This action updates a #${id} specialty`;
+  async update(id: number, updateSpecialtyDto: UpdateSpecialtyDto) {
+    try {
+      const spec = await this.specialtyRepository.findOneBy({ id });
+      if (!spec) {
+        return {
+          statusCode: 2,
+          message: 'Chuyên khoa không tồn tại',
+        };
+      }
+      await this.specialtyRepository.update(id, { ...updateSpecialtyDto });
+      if (updateSpecialtyDto.image && spec.image) {
+        const path = `uploads/specialty/${spec.image}`;
+        if (fs.existsSync(path)) {
+          fs.unlink(path, (err) => {
+            if (err) throw err;
+            console.log('Delete File successfully.');
+          });
+        }
+      }
+      return {
+        statusCode: 0,
+        message: 'Cập nhật chuyên khoa thành công',
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} specialty`;
+  async remove(id: number) {
+    try {
+      if (!id) {
+        return {
+          statusCode: 1,
+          message: 'Thiếu thông tin id',
+        };
+      }
+      const spec = await this.specialtyRepository.findOneBy({ id: id });
+      if (!spec) {
+        return {
+          statusCode: 2,
+          message: 'Không tìm thấy chuyên khoa để xoá',
+        };
+      }
+      if (spec.image) {
+        const path = `uploads/specialty/${spec.image}`;
+        if (fs.existsSync(path)) {
+          fs.unlink(path, (err) => {
+            if (err) throw err;
+            console.log('Delete File successfully.');
+          });
+        }
+      }
+      await this.specialtyRepository.delete({ id: id });
+      return {
+        statusCode: 0,
+        message: 'Xoá người dùng thành công',
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
