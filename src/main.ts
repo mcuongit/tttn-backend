54fefaf7ce6ea5d8dceeb53ddda5a8ce4724088b
mcuongit <cuong2th2@gmail.com>
2023-07-02 21:26:29 +0700

    last cm

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,20 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 5001;
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5000',
-      'http://localhost:5173',
-      process.env.REACT_URL,
-      'https://manhcuong-fe.vercel.app/',
-    ],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000',
+  //     'http://localhost:5000',
+  //     'http://localhost:5173',
+  //     process.env.REACT_URL,
+  //     'https://manhcuong-fe.vercel.app/',
+  //   ],
+  //   credentials: true,
+  // });
   await app.listen(port);
   console.log(`Your server is running on http://localhost:${port}`);
 }
